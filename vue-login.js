a0b49f41557f6d8f9aa575d945a64124795051fe
ShinYi3 <a1847337@adelaide.edu.au>
2023-05-12 07:02:08 +0000

    if user signedin, login button change to account and sign out button

--- vue-login.js
+++ vue-login.js
@@ -1,16 +1,17 @@
 const vueinst = Vue.createApp ({
     data(){
       return{
-        signedIn: false
+        signedIn: false,
+        buttonHover: false
       };
     },
     computed: {
         URL: function(){
             // If user is signed in, change log in button to account button
-            if (this.signedIn === true) {
-                return "account.html";
-            } else {
+            if (this.signedIn === false) {
                 return "login-new.html";
+            } else {
+                return 'account.html';
             }
         },
         buttonName: function() {
