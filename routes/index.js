6e1da2075562b9dd8cf70b74f7283654c1b262d3
ShinYi3 <a1847337@adelaide.edu.au>
2023-05-26 04:55:39 +0000

    connected mysql

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res, next){
+
+  req.pool.getConnection(function(cerr, connection){
+
+    if (cerr){
+      console.log("Connection error");
+      res.sendStatus(500);
+    }
+
+    let query = "SELECT * FROM ADMINS";
+    connection.query(query, function(qerr, rows, fields){
+      connection.release();
+
+      if (qerr){
+        console.log("Query error");
+        res.sendStatus(500);
+      }
+
+      console.log(JSON.stringify(rows));
+
+    });
+  });
+
+});
 module.exports = router;
