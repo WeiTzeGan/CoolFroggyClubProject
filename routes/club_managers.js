6af8e03cd52eaa48d34fb3c0217f7f74e9715374
ShinYi3 <a1847337@adelaide.edu.au>
2023-06-06 06:27:51 +0000

    added email notifications for events

--- routes/club_managers.js
+++ routes/club_managers.js
@@ -120,7 +120,7 @@ router.post('/newAnnouncement', function(req, res, next) {
         }
 
         let query2 = "INSERT INTO ANNOUNCEMENTS (title, post_message, private_message, club_id) VALUES (?, ?, ?, ?)";
-        console.log("sending data");
+
         connection.query(query2, [postTitle, postMessage, privateMessage, clubID], function(error, rows, fields) {
           connection.release();
 
@@ -142,8 +142,8 @@ router.post('/newAnnouncement', function(req, res, next) {
 
 /* Route to send out announcements email notifications */
 router.post('/newsEmail', function(req, res, next) {
-  var data = req.app.locals.tempData;
-  console.log('Received data: ', data);
+  let data = req.app.locals.tempData;
+  req.app.locals.tempData = '';
 
   // Check if announcement exists
   req.pool.getConnection(function(err, connection) {
@@ -163,8 +163,8 @@ router.post('/newsEmail', function(req, res, next) {
         return;
       }
 
-      if (rows.length > 0) {
-        console.log("Announcement title already exists");
+      if (rows.length < 1) {
+        console.log("Announcement doesn't exist");
         res.sendStatus(403);
         return;
       }
@@ -176,7 +176,7 @@ router.post('/newsEmail', function(req, res, next) {
           return;
         }
 
-        let query2 = "SELECT USERS.email, EMAIL_NOTIF.club_id FROM USERS INNER JOIN EMAIL_NOTIF ON USERS.user_id = EMAIL_NOTIF.user_id WHERE EMAIL_NOTIF.club_id = ? AND EMAIL_NOTIF.news_notif = 1";
+        let query2 = "SELECT USERS.email, EMAIL_NOTIF.club_id, CLUBS.club_name FROM ((EMAIL_NOTIF INNER JOIN USERS ON USERS.user_id = EMAIL_NOTIF.user_id) INNER JOIN CLUBS ON EMAIL_NOTIF.club_id = CLUBS.club_id) WHERE EMAIL_NOTIF.club_id = ? AND EMAIL_NOTIF.news_notif = 1";
 
         connection.query(query2, [data.club_id], function(error, rows, fields) {
           connection.release();
@@ -186,13 +186,23 @@ router.post('/newsEmail', function(req, res, next) {
             return;
           }
 
-          var emails = 'gohshinyi03@gmail.com';
+          var emails = '';
+
+          // Iterate over the rows and concatenate email values
+          for (let i = 0; i < rows.length; i++) {
+            emails += rows[i].email + ', ';
+          }
+
+          // Remove the trailing comma and whitespace
+          emails = emails.trim().slice(0, -1);
+
+          console.log(emails);
 
           const mailOptions = {
             from: 'coolfroggyclub@gmail.com',
             to: emails,
-            subject: data.title,
-            text: data.post_message
+            subject: 'Announcement from ' + rows[0].club_name + ' : ' + data.title,
+            text: data.post_message + '\n' + '\n' + 'Warm regards, ' + '\n' + rows[0].club_name
           };
 
           transporter.sendMail(mailOptions, function(error, info) {
@@ -212,7 +222,6 @@ router.post('/newsEmail', function(req, res, next) {
       });
     });
   });
-
 });
 
 /* Router to create new club events */
@@ -266,8 +275,97 @@ router.post('/addEvent', function(req, res, next) {
             return;
           }
 
-          res.sendStatus(200);
+          req.app.locals.tempData = {event_name: eventName, event_message: eventMessage, event_date: eventDate, event_location: eventLocation, club_id: clubID};
+
+          res.redirect(307,'/club_managers/eventsEmail');
+
+          //res.sendStatus(200);
         });
+      });
+    });
+  });
+});
+
+/* Route to send out events email notifications */
+router.post('/eventsEmail', function(req, res, next) {
+  let data = req.app.locals.tempData;
+  req.app.locals.tempData = '';
+
+  // To check if event exists
+  req.pool.getConnection(function(err, connection) {
+    if (err) {
+      console.log("Connection error");
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = "SELECT * FROM EVENTS WHERE event_name = ?";
+
+    connection.query(query, [data.event_name], function(error, rows, fields) {
+      connection.release();
+
+      if (error) {
+        console.log("First query error");
+        res.sendStatus(500);
+        return;
+      }
+
+      if (rows.length < 1) {
+        console.log("Event doesn't exist");
+        res.sendStatus(403);
+        return;
+      }
+
+      // If passes above, send email
+      req.pool.getConnection(function(cerr, connection) {
+        if (cerr) {
+          res.sendStatus(500);
+          return;
+        }
+
+        let query2 = "SELECT USERS.email, EMAIL_NOTIF.club_id, CLUBS.club_name FROM ((EMAIL_NOTIF INNER JOIN USERS ON USERS.user_id = EMAIL_NOTIF.user_id) INNER JOIN CLUBS ON EMAIL_NOTIF.club_id = CLUBS.club_id) WHERE EMAIL_NOTIF.club_id = ? AND EMAIL_NOTIF.news_notif = 1";
+
+        connection.query(query2, [data.club_id], function(error, rows, fields) {
+          connection.release();
+
+          if (error) {
+            res.sendStatus(500);
+            return;
+          }
+
+          var emails = '';
+
+          // Iterate over the rows and concatenate email values
+          for (let i = 0; i < rows.length; i++) {
+            emails += rows[i].email + ', ';
+          }
+
+          // Remove the trailing comma and whitespace
+          emails = emails.trim().slice(0, -1);
+
+          console.log(emails);
+
+          const mailOptions = {
+            from: 'coolfroggyclub@gmail.com',
+            to: emails,
+            subject: 'New event from ' + rows[0].club_name + ' : ' + data.event_name,
+            text: data.event_message + '\n' + 'Date: ' + data.event_date + '\n' + 'Location: ' + data.event_location + '\n' + '\n' + 'Warm regards, ' + '\n' + rows[0].club_name
+          };
+
+          transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+              console.log(error);
+              console.log('Error sending email');
+              res.sendStatus(500);
+            } else {
+              console.log('mail send', info);
+              console.log('Email sent successfully');
+            }
+          });
+
+          console.log("Email successfully sent");
+          res.sendStatus(200);
+      });
       });
     });
   });
